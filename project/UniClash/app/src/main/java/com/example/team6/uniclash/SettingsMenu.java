75c3d69939c94fbb4749ec6a2a1028ee790e3bbe
blydr <blydr@vcu.edu>
2017-03-22 14:44:15 -0400

    changed team select buttons to radio buttons

--- project/UniClash/app/src/main/java/com/example/team6/uniclash/SettingsMenu.java
+++ project/UniClash/app/src/main/java/com/example/team6/uniclash/SettingsMenu.java
@@ -5,14 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 
 public class SettingsMenu extends AppCompatActivity {
     Button mainMenu;
 
     //change to radio buttons?
-    Button ramsButton;
-    Button hokiesButton;
-    Button spidersButton;
+    RadioButton ramsRB;
+    RadioButton hokiesRB;
+    RadioButton spidersRB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +23,9 @@ public class SettingsMenu extends AppCompatActivity {
         mainMenu = (Button) findViewById(R.id.mainMenuButton);
 
         //also radio buttons?
-        ramsButton = (Button) findViewById(R.id.chooseRamsButton);
-        hokiesButton = (Button) findViewById(R.id.chooseHokiesButton);
-        spidersButton = (Button) findViewById(R.id.chooseSpidersButton);
+        ramsRB = (RadioButton) findViewById(R.id.ramsRB);
+        hokiesRB = (RadioButton) findViewById(R.id.hokiesRB);
+        spidersRB = (RadioButton) findViewById(R.id.spidersRB);
     }
 
     //Methods for switching between menus
