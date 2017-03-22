f8f6d5847b44c7fc8cccf976e84e5ac76c0c88cf
yeaplebetamn <yeaplebetamn@mymail.vcu.edu>
2017-03-22 10:31:38 -0400

    messing with Activity switching
    added activity switching intent

--- project/UniClash/app/src/main/java/com/example/team6/uniclash/MainMenu.java
+++ project/UniClash/app/src/main/java/com/example/team6/uniclash/MainMenu.java
@@ -1,7 +1,9 @@
 package com.example.team6.uniclash;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    public boolean onMenuItemSelected(Button PlayButton){
+        Intent levelSelectMenuIntent = new Intent(this, LevelSelectMenu.class);
+        startActivity(levelSelectMenuIntent);
+        return super.onMenuItemSelected(PlayButton);
     }
 }
