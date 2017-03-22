9f6811e89cb67bcc5c1a5e17e21d3d29b358f7fc
Ruchiben Ghelani <ghelanir@vcu.edu>
2017-03-22 16:22:32 -0400

    Testing changes

--- project/UniClash/app/src/androidTest/java/com/example/team6/uniclash/MainMenuTest.java
+++ project/UniClash/app/src/androidTest/java/com/example/team6/uniclash/MainMenuTest.java
@@ -11,30 +11,32 @@ import org.junit.runner.RunWith;
 
 
 import android.support.test.rule.ActivityTestRule;
-import android.widget.Button;
-
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static junit.framework.Assert.assertNotNull;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
 public class MainMenuTest extends Fragment {
 
+
     @Rule
     public ActivityTestRule<MainMenu> mActivityTestRule = new ActivityTestRule<>(MainMenu.class);
-    /*public void displayTest(){
-        //ensure that the UI is displaying correctly
-        ViewInteraction PlayButton = onView(
-        )
-*/
-    @Test
+
+    /* Test help :http://stackoverflow.com/questions/9405561/test-if-a-button-starts-a-new-activity-in-android-junit-pref-without-robotium
+    */
+
+
+  /*  @Test
     public void testOpenNextActivity(){
         // register next activity that need to be monitored.
         Instrumentation.ActivityMonitor activityMonitor = getInstrumentation().addMonitor(LevelSelectMenu.class.getName(), null, false);
 
         // open current activity.
         MainMenu myActivity = (MainMenu) getActivity();
-        final Button button = (Button) myActivity.findViewById(R.id.PlayButton);
+        final Button button = (Button) myActivity.findViewById(com.example.team6.uniclash.R.id.PlayButton);
         myActivity.runOnUiThread(new Runnable() {
             @Override
             public void run() {
@@ -45,10 +47,32 @@ public class MainMenuTest extends Fragment {
 
         //Watch for the timeout
         //example values 5000 if in ms, or 5 if it's in seconds.
-        TeamSelectMenu teamSelectMenuActivity = (TeamSelectMenu) getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 5000);
+            LevelSelectMenu levelSelectMenuActivity = (LevelSelectMenu) getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 100000);
         // next activity is opened and captured.
-        assertNotNull(teamSelectMenuActivity);
-        teamSelectMenuActivity.finish();
+        assertNotNull(levelSelectMenuActivity);
+        levelSelectMenuActivity.finish();
+    }
+*/
+    @Test
+
+      public void testPlayButton()
+        {
+            Instrumentation.ActivityMonitor activityMonitor = getInstrumentation().addMonitor(LevelSelectMenu.class.getName(), null, false);
+            onView(withId(R.id.PlayButton)).perform(click());
+            LevelSelectMenu levelSelectMenuActivity = (LevelSelectMenu) getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 5);
+            assertNotNull(levelSelectMenuActivity);
+            levelSelectMenuActivity.finish();
+        }
+
+    @Test
+
+    public void testSettingsButton()
+    {
+        Instrumentation.ActivityMonitor activityMonitor = getInstrumentation().addMonitor(SettingsMenu.class.getName(), null, false);
+        onView(withId(R.id.SettingsButton)).perform(click());
+        SettingsMenu levelSelectMenuActivity = (SettingsMenu) getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 5);
+        assertNotNull(levelSelectMenuActivity);
+        levelSelectMenuActivity.finish();
     }
 }
 
