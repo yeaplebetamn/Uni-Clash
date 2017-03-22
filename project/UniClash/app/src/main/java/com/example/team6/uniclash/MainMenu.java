ace347149c2ce7df73f50068173cb4ba0746c098
yeaplebetamn <yeaplebetamn@mymail.vcu.edu>
2017-03-22 12:21:53 -0400

    Added activity switching method from Android Dev website
    https://developer.android.com/training/basics/firstapp/starting-activity.html

--- project/UniClash/app/src/main/java/com/example/team6/uniclash/MainMenu.java
+++ project/UniClash/app/src/main/java/com/example/team6/uniclash/MainMenu.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
@@ -15,25 +16,32 @@ public class MainMenu extends AppCompatActivity {
 
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-
-        //stack overflow's method of activity switching
-        switch(item.getItemId()){
-            case R.id.PlayButton:
-                startActivity(new Intent(this, LevelSelectMenu.class));
-                return true;
-            case R.id.SettingsButton:
-                startActivity(new Intent(this, SettingsMenu.class));
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-
-        }
-
-        //Duke's method of activity switching
-//        Intent levelSelectMenuIntent = new Intent(this, LevelSelectMenu.class);
-//        startActivity(levelSelectMenuIntent);
-//        return super.onOptionsItemSelected(item);
+    //Method based on: https://developer.android.com/training/basics/firstapp/starting-activity.html
+    public void pressPlay(View view){
+        Intent levelSelectMenuIntent = new Intent(this, LevelSelectMenu.class);
+        startActivity(levelSelectMenuIntent);
     }
+
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item){
+//
+//        //stack overflow's method of activity switching
+//        switch(item.getItemId()){
+//            case R.id.PlayButton:
+//                startActivity(new Intent(this, LevelSelectMenu.class));
+//                return true;
+//            case R.id.SettingsButton:
+//                startActivity(new Intent(this, SettingsMenu.class));
+//                return true;
+//            default:
+//                return super.onOptionsItemSelected(item);
+//
+//        }
+//
+//        //Duke's method of activity switching
+////        Intent levelSelectMenuIntent = new Intent(this, LevelSelectMenu.class);
+////        startActivity(levelSelectMenuIntent);
+////        return super.onOptionsItemSelected(item);
+//    }
 }
