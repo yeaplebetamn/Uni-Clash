eb4766657284fa56832f0b5a2346c3bd8b2da714
blydr <blydr@vcu.edu>
2017-03-31 15:11:08 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    
    #	project/UniClash/app/src/main/java/com/example/team6/uniclash/GameView.java
    #	project/UniClash/app/src/main/res/layout/activity_game_view.xml

--- project/UniClash/app/src/main/java/com/example/team6/uniclash/GameActivity.java
+++ project/UniClash/app/src/main/java/com/example/team6/uniclash/GameActivity.java
@@ -25,7 +25,7 @@ public class GameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game_view);
+        setContentView(R.layout.activity_game);
 
         gameSurfaceView = new GameView(this);
         
