39fd06c70ec61e4f71928675439ddfcd9014e690
blydr <blydr@vcu.edu>
2017-03-31 16:44:45 -0400

    oops?

--- project/UniClash/app/src/main/java/com/example/team6/uniclash/DefaultEnemy.java
+++ project/UniClash/app/src/main/java/com/example/team6/uniclash/DefaultEnemy.java
@@ -1,9 +1,15 @@
 package com.example.team6.uniclash;
 
+import android.content.Context;
+import android.graphics.BitmapFactory;
+
 public class DefaultEnemy extends Enemy {
-    public DefaultEnemy() {
+    public DefaultEnemy(Context context, int screenX, int screenY) {
         setHealth(100);
         setSpeed(2);
         setAttack(3);
+        setBitmap(BitmapFactory.decodeResource(context.getResources(), R.drawable.clash_of_kings_college));
+        setX(75);
+        setY(50);
     }
 }
