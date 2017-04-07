804d4107c2d821c07ab9d8f98974ecc71696213e
blydr <blydr@vcu.edu>
2017-04-07 19:45:21 -0400

    Back button crashes slightly more gracefully

--- project/UniClash/app/src/main/java/com/example/team6/uniclash/GameActivity.java
+++ project/UniClash/app/src/main/java/com/example/team6/uniclash/GameActivity.java
@@ -199,4 +199,30 @@ public class GameActivity extends AppCompatActivity {
         gameView.resume();
     }
 
+
+    //causes crash :(
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Are you sure you want to exit?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                        Intent startMain = new Intent(Intent.ACTION_MAIN);
+                        startMain.addCategory(Intent.CATEGORY_HOME);
+                        startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(startMain);
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+
+    }
 }
