790dab62b79c5addc0914fc66d4396dbab9ba7f0
blydr <blydr@vcu.edu>
2017-04-07 16:59:11 -0400

    updated turret attacking logic
    only attacks one enemy at a time

--- project/UniClash/app/src/main/java/com/example/team6/uniclash/FreezeTower.java
+++ project/UniClash/app/src/main/java/com/example/team6/uniclash/FreezeTower.java
@@ -1,5 +1,7 @@
 package com.example.team6.uniclash;
 
+import java.util.ArrayList;
+
 //slows enemies in range, no damage.
 public class FreezeTower extends Tower {
     public FreezeTower() {
@@ -7,5 +9,14 @@ public class FreezeTower extends Tower {
         setAttackDelay(0);
         setRange(100);
         setRangeDetector(getRange());
+    }
+
+    @Override
+    public void attack(Enemy target, ArrayList<Enemy> enemies) {
+        for (Enemy enemy: enemies) {
+            if (getRangeDetector().intersect(enemy.getCollisionDetector())) {
+                //enemy = slowed
+            }
+        }
     }
 }
